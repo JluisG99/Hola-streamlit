9425895150131f6f0e3aa6fba52310559b1744a4
JluisG99 <luisgamboa376@gmail.com>
2022-11-21 11:55:09 -0600

    Agregar línea 2

---
+++ principal.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+
+st.title("Hola mundo")
+
+st.write("Hola mundo")
+
+st.header("Primer encabezado")
+
+st.subheader("Segundo encabezado")
+
+st.markdown("_itálica_, **negrita**, [UCR](https://www.bing.com/search?q=UCR&cvid=21f4774904b84ff7934e08ce6d2b0c67&aqs=edge..69i57j0l5j69i60l3.1631j0j1&pglt=43&FORM=ANNTA1&PC=U531)")
